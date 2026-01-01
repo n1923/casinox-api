41563ef866fd9dbef18eaa661401780a057048b5
n1923 <zonelix@proton.me>
2026-01-02 02:17:54 +0400

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -10,7 +10,7 @@ export async function connectToDatabase(): Promise<{ client: MongoClient; db: Db
   if (!MONGODB_URI) {
     throw new Error('MONGODB_URI is not defined in environment variables');
   }
-  
+
   if (!MONGODB_DB) {
     throw new Error('MONGODB_DB is not defined in environment variables');
   }
@@ -21,9 +21,9 @@ export async function connectToDatabase(): Promise<{ client: MongoClient; db: Db
 
   const client = await MongoClient.connect(MONGODB_URI);
   const db = client.db(MONGODB_DB);
-  
+
   cachedClient = client;
   cachedDb = db;
-  
+
   return { client, db };
 }
